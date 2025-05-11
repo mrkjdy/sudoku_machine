8565193d7742b3cfd47138b6879aa87c1409fe95
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2025-05-11 18:30:33 -0400

    chore: upgrade to bevy 0.16 (#63)

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@ pub enum AppState {
 // Generic system that takes a component as a parameter, and will despawn all entities with that component
 pub fn despawn_component<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
     for entity in &to_despawn {
-        commands.entity(entity).despawn_recursive();
+        commands.entity(entity).despawn();
     }
 }
 
