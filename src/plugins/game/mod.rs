c86a59e2290be045be84714a2be7026775fd3572
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2025-11-08 18:39:27 -0500

    chore: disable non-classic modes for now (#110)
    Will reenable them when they're ready!

--- src/plugins/game/mod.rs
+++ src/plugins/game/mod.rs
@@ -6,7 +6,9 @@ use strum_macros::{Display, EnumIter};
 use crate::{AppState, PuzzleSettings};
 
 mod classic;
+#[cfg(debug_assertions)]
 mod full_kropki;
+#[cfg(debug_assertions)]
 mod knight;
 
 #[derive(Default, EnumIter, Display, TryFromPrimitive, Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -14,7 +16,9 @@ mod knight;
 pub enum PuzzleType {
     #[default]
     Classic,
+    #[cfg(debug_assertions)]
     Knight,
+    #[cfg(debug_assertions)]
     #[strum(to_string = "Full Kropki")]
     FullKropki,
 }
@@ -27,9 +31,11 @@ impl PuzzleType {
                 Fill a 9x9 grid so each row, column, and 3x3 box contains all digits 1-9 without \
                 repetition.
             "},
+            #[cfg(debug_assertions)]
             PuzzleType::Knight => indoc! {"
                 Classic rules plus no identical numbers can be a knight’s move apart, like in chess.
             "},
+            #[cfg(debug_assertions)]
             PuzzleType::FullKropki => indoc! {"
                 Classic rules plus Kropki dot rules for adjacent cells:
                  • a black dot means one of the numbers is twice the value of the other
@@ -52,7 +58,9 @@ pub fn game_plugin(app: &mut App) {
         .add_systems(OnEnter(AppState::Game), game_setup)
         .add_plugins((
             classic::classic_plugin,
+            #[cfg(debug_assertions)]
             full_kropki::full_kropki_plugin,
+            #[cfg(debug_assertions)]
             knight::knight_plugin,
         ));
 }
